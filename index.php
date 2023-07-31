4fbfebee61910c4af93a196a2f277267bb6a8393
Jessy Marion <jessymarionmail@gmail.com>
2023-07-31 17:01:13 +0200

    feat: logique form

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+
+const ERROR_REQUIRED = "Veuillez renseigner une todo";
+const ERROR_TOO_SHORT = "Veuillez enter au moins 5 caracteres";
+$error = "";
+
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
+    $todo = $_POST["todo"] ?? "";
+
+    if (!$todo) {
+        $error = ERROR_REQUIRED;
+    } else if (mb_strlen($todo) < 5) {
+        $error = ERROR_TOO_SHORT;
+    }
+
+};
+
+?>
+
+
 <!doctype html>
 <html lang="fr">
 <head>
@@ -14,9 +36,12 @@
         <div class="todo-container">
             <h1>Ma Todo</h1>
             <form action="/" method="POST" class="todo-form">
-                <input type="text">
+                <input name="todo" type="text">
                 <button class="btn btn-primary">Ajouter</button>
             </form>
+            <?php if ($error): ?>
+                <p class="text-danger"><?= $error ?></p>
+            <?php endif; ?>
             <div class="todo-list"></div>
         </div>
     </div>
