7c6d6c7a25b84c4642dc2392cb0fa0c0334bd1cc
Jessy Marion <jessymarionmail@gmail.com>
2023-07-31 18:22:24 +0200

    style: style improvement

--- index.php
+++ index.php
@@ -1,25 +1,23 @@
 <?php
-
-const ERROR_REQUIRED = "Veuillez renseigner une todo";
-const ERROR_TOO_SHORT = "Veuillez enter au moins 5 caracteres";
-
+const ERROR_REQUIRED = 'Veuillez renseigner une todo';
+const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractères';
 $filename = __DIR__ . "/data/todos.json";
-
-$error = "";
+$error = '';
 $todos = [];
 
-
 if (file_exists($filename)) {
-
     $data = file_get_contents($filename);
     $todos = json_decode($data, true) ?? [];
-
 }
 
-
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS);
-    $todo = $_POST["todo"] ?? "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_POST = filter_input_array(INPUT_POST, [
+        "todo" => [
+            "filter" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+            "flags" => FILTER_FLAG_NO_ENCODE_QUOTES | FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_BACKTICK
+        ]
+    ]);
+    $todo = $_POST['todo'] ?? '';
 
     if (!$todo) {
         $error = ERROR_REQUIRED;
@@ -28,18 +26,14 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     }
 
     if (!$error) {
-
         $todos = [...$todos, [
-            "name" => $todo,
-            "done" => false,
-            "id" => time(),
+            'name' => $todo,
+            'done' => false,
+            'id' => time()
         ]];
-        file_put_contents($filename, json_encode($todos));
-
+        file_put_contents($filename, json_encode($todos, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
     }
-
-};
-
+}
 ?>
 
 
@@ -65,7 +59,15 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
             <?php if ($error): ?>
                 <p class="text-danger"><?= $error ?></p>
             <?php endif; ?>
-            <div class="todo-list"></div>
+            <ul class="todo-list">
+                <?php foreach ($todos as $t): ?>
+                    <li class="todo-item">
+                        <span class="todo-name"><?= $t["name"] ?></span>
+                        <button class="btn btn-primary btn-small">Valider</button>
+                        <button class="btn btn-primary btn-small">Supprimer</button>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
     </div>
 
